a6e9861d7ffda31cc097df96f2d59cc5c77024d0
pycckuu <is.markelov@gmail.com>
2014-05-17 01:25:42 +0200

    1d case in python

--- bvp_ode.py
+++ bvp_ode.py
@@ -10,6 +10,9 @@ class BvpOde:
         self.num_nodes = num_nodes
         self.grid  = self.form_grid()
         self.populate_matrix()
+        self.populate_vector()
+        self.apply_boundary_conditions()
+        self.solve()
 
     def form_grid(self):
         return {'x': np.linspace(self.ode.x_min, self.ode.x_max, num=self.num_nodes)}
@@ -22,8 +25,6 @@ class BvpOde:
         xm = x_vec[0:-2]
         x = x_vec[1:-1]
         xp = x_vec[2:]
-        print xm, x , xp
-
 
         diffusion_alpha = 2.0/(xp-xm)/(x-xm)
         diffusion_betta = -2.0/(xp-x)/(x-xm)
@@ -32,9 +33,48 @@ class BvpOde:
         main_diag = diffusion_betta*self.ode.uxx + self.ode.u
         left_diag = diffusion_alpha*self.ode.uxx - self.ode.ux/(xp-xm)
         right_diag = diffusion_gamma*self.ode.uxx + self.ode.ux/(xp-xm)
-        main_diag = np.append(0, main_diag); left_diag = np.append(left_diag, 0); right_diag = np.append(0, right_diag);
-        print  main_diag, left_diag, right_diag
-        mtx = sparse.spdiags([left_diag,main_diag ,right_diag ], [-1,0, 1], self.num_nodes, self.num_nodes)
-        print mtx.todense()
+        main_diag = np.append([0], main_diag, 0)
+        main_diag = np.append(main_diag,[0], 0)
+        left_diag = np.append(left_diag, [[0, 0]]); 
+        right_diag = np.append([0, 0], right_diag);
+        self.mtx = sparse.spdiags([left_diag, main_diag, right_diag ], [-1,0, 1], self.num_nodes, self.num_nodes, format="lil")
+
+    def populate_vector(self):
+        x_vec = self.grid['x']
+        self.vec = [self.ode.rhs_func(x) for x in x_vec]
+
+    def apply_boundary_conditions(self):
+        left_bc_applied = False; 
+        right_bc_applied = False;
+
+        if self.bc.x0_bc_is_dirichlet:
+            self.mtx[0,0] = 1
+            left_bc_applied = True
+
+        if self.bc.xn_bc_is_dirichlet:
+            self.mtx[self.num_nodes-1,self.num_nodes-1] = 1
+            right_bc_applied = True
+
+        if self.bc.x0_bc_is_neumann:
+            h = self.grid['x'][1]-self.grid['x'][0]
+            self.mtx[0,0] = -1.0/h
+            self.mtx[0,1] = 1/h
+            left_bc_applied = True
+
+        if self.bc.xn_bc_is_neumann:
+            h = self.grid['x'][self.num_nodes-1]-self.grid['x'][self.num_nodes-2]
+            self.mtx[self.num_nodes-1, self.num_nodes-2] = -1.0/h
+            self.mtx[self.num_nodes-1, self.num_nodes-1] = 1/h
+            right_bc_applied = True
+        self.vec[0] = self.bc.x0_value
+        self.vec[self.num_nodes-1] = self.bc.xn_value
+        assert right_bc_applied and left_bc_applied
+
+    def solve(self):
+        self.mtx = sparse.csr_matrix(self.mtx)
+        self.mtx = self.mtx.astype(np.float64)
+        self.x = dsolve.spsolve(self.mtx, np.array(self.vec), use_umfpack=True)
+
+        
 
         
--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from second_order_ode import *
 from boundary_conditions import *
 from bvp_ode import *
+import matplotlib.pyplot as plt
+import math
 
 def model_prob_1(x):
     return 1.0
+def model_prob_2(x):
+    return 34* math.sin(x)
+# ode = SecondOrderOde(-1, 0, 0, model_prob_1, 0 , 1)
+# bc = BoundaryConditions()
+# bc.set_x0_dirichlet_bc(0)
+# bc.set_xn_dirichlet_bc(0)
+# bvp = BvpOde(ode, bc, 1000)
+# bvp.set_file_name('att.dat')
 
-ode = SecondOrderOde(-1, 0, 0, model_prob_1, 0 , 1)
-bc = BoundaryConditions()
-bc.set_x0_dirichlet_bc(0)
-bc.set_xn_dirichlet_bc(0)
+for i in xrange(1,1000):
+    ode2 = SecondOrderOde(1, 3, -4, model_prob_2, 0, math.pi)
+    bc2 = BoundaryConditions()
+    bc2.set_x0_neumann_bc(-5)
+    bc2.set_xn_dirichlet_bc(4)
+    bvp2 = BvpOde(ode2,bc2, 1000)
 
-bvp = BvpOde(ode, bc, 4)
-bvp.set_file_name('att.dat')
+# print bvp2.x
+
+# plt.plot(bvp2.grid['x'],bvp2.x)
+# plt.show()
 
 # print bvp.__dict__
 
