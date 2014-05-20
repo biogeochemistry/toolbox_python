f598dd793c39677f093d8c87e4631acbdca75512
pycckuu <is.markelov@gmail.com>
2014-05-20 13:01:07 +0200

    2d start point

--- bvp_ode.py
+++ bvp_ode.py
@@ -11,12 +11,12 @@ class BvpOde:
         self.file_name = str    
 
     def solve(self):
-        self.populate_matric()
+        self.populate_matrix()
         self.populate_vector()
         self.apply_boundary_conditions()
-        self.mtc = sparse.csr_matrix(self.mtc)
-        self.mtc = self.mtc.astype(np.float64)
-        self.U = dsolve.spsolve(self.mtc, np.array(self.vec), use_umfpack=True)
+        self.mtx = sparse.csr_matrix(self.mtx)
+        self.mtx = self.mtx.astype(np.float64)
+        self.U = dsolve.spsolve(self.mtx, np.array(self.vec), use_umfpack=True)
 
     def make_x_grid(self):
         self.grid_x  = np.linspace(self.ode.x_min, self.ode.x_max, num=self.num_x_nodes)
@@ -36,7 +36,7 @@ class BvpOde1D(BvpOde):
     def set_file_name(self, str):
         self.file_name = str
 
-    def populate_matric(self):
+    def populate_matrix(self):
         x_vec = self.grid_x
         xm = x_vec[0:-2]
         x = x_vec[1:-1]
@@ -53,7 +53,7 @@ class BvpOde1D(BvpOde):
         main_diag = np.append(main_diag,[0], 0)
         left_diag = np.append(left_diag, [[0, 0]]); 
         right_diag = np.append([0, 0], right_diag);
-        self.mtc = sparse.spdiags([left_diag, main_diag, right_diag ], [-1,0, 1], self.num_x_nodes, self.num_x_nodes, format="lil")
+        self.mtx = sparse.spdiags([left_diag, main_diag, right_diag ], [-1,0, 1], self.num_x_nodes, self.num_x_nodes, format="lil")
 
     def populate_vector(self):
         x_vec = self.grid_x
@@ -64,29 +64,38 @@ class BvpOde1D(BvpOde):
         right_bc_applied = False;
 
         if self.bc.x0_is_dirichlet:
-            self.mtc[0,0] = 1
+            self.mtx[0,0] = 1
             left_bc_applied = True
 
         if self.bc.xn_is_dirichlet:
-            self.mtc[self.num_x_nodes-1,self.num_x_nodes-1] = 1
+            self.mtx[self.num_x_nodes-1,self.num_x_nodes-1] = 1
             right_bc_applied = True
 
         if self.bc.x0_is_neumann:
             h = self.grid_x[1]-self.grid_x[0]
-            self.mtc[0,0] = -1.0/h
-            self.mtc[0,1] = 1.0/h
+            self.mtx[0,0] = -1.0/h
+            self.mtx[0,1] = 1.0/h
             left_bc_applied = True
 
         if self.bc.xn_is_neumann:
             h = self.grid_x[self.num_x_nodes-1]-self.grid_x[self.num_nodes-2]
-            self.mtc[self.num_nodes-1, self.num_nodes-2] = -1.0/h
-            self.mtc[self.num_nodes-1, self.num_nodes-1] = 1.0/h
+            self.mtx[self.num_nodes-1, self.num_nodes-2] = -1.0/h
+            self.mtx[self.num_nodes-1, self.num_nodes-1] = 1.0/h
             right_bc_applied = True
         self.vec[0] = self.bc.x0_value
         self.vec[self.num_x_nodes-1] = self.bc.xn_value
         assert right_bc_applied and left_bc_applied
 
+class BvpOde2D(BvpOde1D):
+    """docstring for BvpOde2D"""
+    def __init__(self, ode, bc, num_x_nodes, num_y_nodes):
+        BvpOde1D.__init__(self, ode, bc, num_x_nodes)
+        self.num_y_nodes = num_y_nodes
+        self.make_y_grid()
 
+    def populate_matrix(self):
+        pass
+        
         
 
         
