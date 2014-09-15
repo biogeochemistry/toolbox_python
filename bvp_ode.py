7073a74a78e5b55be4a29ec41e54d57e1caf6241
pycckuu <is.markelov@gmail.com>
2014-09-16 01:01:46 +0200

    Updated tests coverage

--- bvp_ode.py
+++ bvp_ode.py
@@ -5,11 +5,12 @@ from scipy.sparse.linalg import dsolve
 
 
 class BvpOde:
+
     """docstring for BvpOde
     This parent class for all 1d 2d 3d bvp problems, Consist of methods which are common for all"""
 
     def set_file_name(self, str):
-        self.file_name = str    
+        self.file_name = str
 
     def solve(self):
         self.populate_matrix_6th_order() if self.uniform == True else self.populate_matrix()
@@ -20,13 +21,16 @@ class BvpOde:
         self.U = dsolve.spsolve(self.mtx, np.array(self.vec), use_umfpack=True)
 
     def make_x_grid(self):
-        self.grid_x  = np.linspace(self.ode.x_min, self.ode.x_max, num=self.num_x_nodes)
+        self.grid_x = np.linspace(self.ode.x_min, self.ode.x_max, num=self.num_x_nodes)
 
     def make_y_grid(self):
-        self.grid_y  = np.linspace(self.ode.y_min, self.ode.y_max, num=self.num_y_nodes)
+        self.grid_y = np.linspace(self.ode.y_min, self.ode.y_max, num=self.num_y_nodes)
+
 
 class BvpOde1D(BvpOde):
+
     """docstring for BvpOde1D"""
+
     def __init__(self, ode, bc, num_x_nodes, custom_grid=False, grid_x=False):
         self.ode = ode
         self.bc = bc
@@ -47,85 +51,82 @@ class BvpOde1D(BvpOde):
         x = x_vec[1:-1]
         xp = x_vec[2:]
 
-        alpha = 2.0/(xp-xm)/(x-xm)
-        betta = -2.0/(xp-x)/(x-xm)
-        gamma = 2.0/(xp-xm)/(xp-x)
-        
-    
-        left_diag  = alpha*self.ode.uxx - self.ode.ux/(xp-xm)
-        main_diag  = betta*self.ode.uxx + self.ode.u
-        right_diag = gamma*self.ode.uxx + self.ode.ux/(xp-xm)
-        main_diag  = np.append([0], main_diag, 0)
-        main_diag  = np.append(main_diag,[0], 0)
-        left_diag  = np.append(left_diag, [[0, 0]]); 
-        right_diag = np.append([0, 0], right_diag);
-        self.mtx   = sparse.spdiags([left_diag, main_diag, right_diag ], [-1,0, 1], self.num_x_nodes, self.num_x_nodes, format="lil")
+        alpha = 2.0 / (xp - xm) / (x - xm)
+        betta = -2.0 / (xp - x) / (x - xm)
+        gamma = 2.0 / (xp - xm) / (xp - x)
+
+        left_diag = alpha * self.ode.uxx - self.ode.ux / (xp - xm)
+        main_diag = betta * self.ode.uxx + self.ode.u
+        right_diag = gamma * self.ode.uxx + self.ode.ux / (xp - xm)
+        main_diag = np.append([0], main_diag, 0)
+        main_diag = np.append(main_diag, [0], 0)
+        left_diag = np.append(left_diag, [[0, 0]])
+        right_diag = np.append([0, 0], right_diag)
+        self.mtx = sparse.spdiags([left_diag, main_diag, right_diag], [-1, 0, 1], self.num_x_nodes, self.num_x_nodes, format="lil")
 
     def populate_matrix_6th_order(self):
-        h = self.grid_x[self.num_x_nodes-1]-self.grid_x[self.num_x_nodes-2]
+        h = self.grid_x[self.num_x_nodes - 1] - self.grid_x[self.num_x_nodes - 2]
         D = self.ode.uxx
         w = self.ode.ux
         k = self.ode.u
-        self.mtx = sparse.lil_matrix( (self.num_x_nodes, self.num_x_nodes) )
+        self.mtx = sparse.lil_matrix((self.num_x_nodes, self.num_x_nodes))
 
         i1 = 1
-        self.mtx[i1,i1+6] =   11*D/(180*h*h)
-        self.mtx[i1,i1+5] =  -90*D/(180*h*h) +   (+2*w)/(60*h)
-        self.mtx[i1,i1+4] =  324*D/(180*h*h) +  (-15*w)/(60*h)
-        self.mtx[i1,i1+3] = -670*D/(180*h*h) +  (+50*w)/(60*h)
-        self.mtx[i1,i1+2] =  855*D/(180*h*h) + (-100*w)/(60*h)
-        self.mtx[i1,i1+1] = -486*D/(180*h*h) + (+150*w)/(60*h)
-        self.mtx[i1,i1]   =  -70*D/(180*h*h) +  (-77*w)/(60*h) + k
-        self.mtx[i1,i1-1] =  126*D/(180*h*h) +  (-10*w)/(60*h)
+        self.mtx[i1, i1 + 6] = 11 * D / (180 * h * h)
+        self.mtx[i1, i1 + 5] = -90 * D / (180 * h * h) + (+2 * w) / (60 * h)
+        self.mtx[i1, i1 + 4] = 324 * D / (180 * h * h) + (-15 * w) / (60 * h)
+        self.mtx[i1, i1 + 3] = -670 * D / (180 * h * h) + (+50 * w) / (60 * h)
+        self.mtx[i1, i1 + 2] = 855 * D / (180 * h * h) + (-100 * w) / (60 * h)
+        self.mtx[i1, i1 + 1] = -486 * D / (180 * h * h) + (+150 * w) / (60 * h)
+        self.mtx[i1, i1] = -70 * D / (180 * h * h) + (-77 * w) / (60 * h) + k
+        self.mtx[i1, i1 - 1] = 126 * D / (180 * h * h) + (-10 * w) / (60 * h)
 
         i2 = 2
-        self.mtx[i2,i2+5] =  -2*D/(180*h*h)
-        self.mtx[i2,i2+4] =  +16*D/(180*h*h) +    (-w)/(60*h)
-        self.mtx[i2,i2+3] =  -54*D/(180*h*h) +  (+8*w)/(60*h)
-        self.mtx[i2,i2+2] =  +85*D/(180*h*h) + (-30*w)/(60*h)
-        self.mtx[i2,i2+1] = +130*D/(180*h*h) + (+80*w)/(60*h)
-        self.mtx[i2,i2]   = -378*D/(180*h*h) + (-35*w)/(60*h) + k
-        self.mtx[i2,i2-1] = +214*D/(180*h*h) + (-24*w)/(60*h)
-        self.mtx[i2,i2-2] =  -11*D/(180*h*h) +  (+2*w)/(60*h)
+        self.mtx[i2, i2 + 5] = -2 * D / (180 * h * h)
+        self.mtx[i2, i2 + 4] = +16 * D / (180 * h * h) + (-w) / (60 * h)
+        self.mtx[i2, i2 + 3] = -54 * D / (180 * h * h) + (+8 * w) / (60 * h)
+        self.mtx[i2, i2 + 2] = +85 * D / (180 * h * h) + (-30 * w) / (60 * h)
+        self.mtx[i2, i2 + 1] = +130 * D / (180 * h * h) + (+80 * w) / (60 * h)
+        self.mtx[i2, i2] = -378 * D / (180 * h * h) + (-35 * w) / (60 * h) + k
+        self.mtx[i2, i2 - 1] = +214 * D / (180 * h * h) + (-24 * w) / (60 * h)
+        self.mtx[i2, i2 - 2] = -11 * D / (180 * h * h) + (+2 * w) / (60 * h)
 
+        im2 = self.num_x_nodes - 3
+        self.mtx[im2, im2 - 5] = -2 * D / (180 * h * h)
+        self.mtx[im2, im2 - 4] = +16 * D / (180 * h * h) + (+w) / (60 * h)
+        self.mtx[im2, im2 - 3] = -54 * D / (180 * h * h) + (-8 * w) / (60 * h)
+        self.mtx[im2, im2 - 2] = +85 * D / (180 * h * h) + (+30 * w) / (60 * h)
+        self.mtx[im2, im2 - 1] = +130 * D / (180 * h * h) + (-80 * w) / (60 * h)
+        self.mtx[im2, im2] = -378 * D / (180 * h * h) + (+35 * w) / (60 * h) + k
+        self.mtx[im2, im2 + 1] = +214 * D / (180 * h * h) + (+24 * w) / (60 * h)
+        self.mtx[im2, im2 + 2] = -11 * D / (180 * h * h) + (-2 * w) / (60 * h)
 
-        im2 = self.num_x_nodes-3
-        self.mtx[im2,im2-5] =  -2*D/(180*h*h)
-        self.mtx[im2,im2-4] =  +16*D/(180*h*h) +    (+w)/(60*h)
-        self.mtx[im2,im2-3] =  -54*D/(180*h*h) +  (-8*w)/(60*h)
-        self.mtx[im2,im2-2] =  +85*D/(180*h*h) + (+30*w)/(60*h)
-        self.mtx[im2,im2-1] = +130*D/(180*h*h) + (-80*w)/(60*h)
-        self.mtx[im2,im2]   = -378*D/(180*h*h) + (+35*w)/(60*h) + k
-        self.mtx[im2,im2+1] = +214*D/(180*h*h) + (+24*w)/(60*h)
-        self.mtx[im2,im2+2] =  -11*D/(180*h*h) +  (-2*w)/(60*h)
+        im1 = self.num_x_nodes - 2
+        self.mtx[im1, im1 - 6] = 11 * D / (180 * h * h)
+        self.mtx[im1, im1 - 5] = -90 * D / (180 * h * h) + (-2 * w) / (60 * h)
+        self.mtx[im1, im1 - 4] = 324 * D / (180 * h * h) + (+15 * w) / (60 * h)
+        self.mtx[im1, im1 - 3] = -670 * D / (180 * h * h) + (-50 * w) / (60 * h)
+        self.mtx[im1, im1 - 2] = 855 * D / (180 * h * h) + (100 * w) / (60 * h)
+        self.mtx[im1, im1 - 1] = -486 * D / (180 * h * h) + (-150 * w) / (60 * h)
+        self.mtx[im1, im1] = -70 * D / (180 * h * h) + (+77 * w) / (60 * h) + k
+        self.mtx[im1, im1 + 1] = 126 * D / (180 * h * h) + (+10 * w) / (60 * h)
 
-        im1 = self.num_x_nodes-2
-        self.mtx[im1,im1-6] =   11*D/(180*h*h)
-        self.mtx[im1,im1-5] =  -90*D/(180*h*h) +   (-2*w)/(60*h)
-        self.mtx[im1,im1-4] =  324*D/(180*h*h) +  (+15*w)/(60*h)
-        self.mtx[im1,im1-3] = -670*D/(180*h*h) +  (-50*w)/(60*h)
-        self.mtx[im1,im1-2] =  855*D/(180*h*h) +  (100*w)/(60*h)
-        self.mtx[im1,im1-1] = -486*D/(180*h*h) + (-150*w)/(60*h)
-        self.mtx[im1,im1]   =  -70*D/(180*h*h) +  (+77*w)/(60*h) + k
-        self.mtx[im1,im1+1] =  126*D/(180*h*h) +  (+10*w)/(60*h)
+        for i in xrange(3, self.num_x_nodes - 3):
+            diffusion_alpha_m3 = 2 * D / (180 * h * h) + (-w) / (60 * h)
+            diffusion_alpha_m2 = -27 * D / (180 * h * h) + (+9 * w) / (60 * h)
+            diffusion_alpha_m1 = 270 * D / (180 * h * h) + (-45 * w) / (60 * h)
+            diffusion_alpha_0 = -490 * D / (180 * h * h) + k
+            diffusion_alpha_p1 = 270 * D / (180 * h * h) + (+45 * w) / (60 * h)
+            diffusion_alpha_p2 = -27 * D / (180 * h * h) + (-9 * w) / (60 * h)
+            diffusion_alpha_p3 = 2 * D / (180 * h * h) + (+w) / (60 * h)
 
-
-        for i in xrange(3,self.num_x_nodes-3):
-            diffusion_alpha_m3 =   2*D/(180*h*h) +   (-w)/(60*h)
-            diffusion_alpha_m2 = -27*D/(180*h*h) + (+9*w)/(60*h)
-            diffusion_alpha_m1 = 270*D/(180*h*h) +(-45*w)/(60*h)
-            diffusion_alpha_0  =-490*D/(180*h*h) + k
-            diffusion_alpha_p1 = 270*D/(180*h*h) +(+45*w)/(60*h)
-            diffusion_alpha_p2 = -27*D/(180*h*h) + (-9*w)/(60*h)
-            diffusion_alpha_p3 =   2*D/(180*h*h) +   (+w)/(60*h)
-
-            self.mtx[i,i-3] = diffusion_alpha_m3
-            self.mtx[i,i-2] = diffusion_alpha_m2
-            self.mtx[i,i-1] = diffusion_alpha_m1
-            self.mtx[i,i]   = diffusion_alpha_0
-            self.mtx[i,i+1] = diffusion_alpha_p1
-            self.mtx[i,i+2] = diffusion_alpha_p2
-            self.mtx[i,i+3] = diffusion_alpha_p3
+            self.mtx[i, i - 3] = diffusion_alpha_m3
+            self.mtx[i, i - 2] = diffusion_alpha_m2
+            self.mtx[i, i - 1] = diffusion_alpha_m1
+            self.mtx[i, i] = diffusion_alpha_0
+            self.mtx[i, i + 1] = diffusion_alpha_p1
+            self.mtx[i, i + 2] = diffusion_alpha_p2
+            self.mtx[i, i + 3] = diffusion_alpha_p3
 
     def populate_vector(self):
         x_vec = self.grid_x
@@ -136,35 +137,38 @@ class BvpOde1D(BvpOde):
         w = self.ode.ux
 
         if self.bc.x0_is_dirichlet:
-            self.mtx[0,0] = 1
+            self.mtx[0, 0] = 1
             self.vec[0] = self.bc.x0_value
             self.is_x0_bc_applied = True
 
         if self.bc.xn_is_dirichlet:
-            self.mtx[self.num_x_nodes-1,self.num_x_nodes-1] = 1
-            self.vec[self.num_x_nodes-1] = self.bc.xn_value
+            self.mtx[self.num_x_nodes - 1, self.num_x_nodes - 1] = 1
+            self.vec[self.num_x_nodes - 1] = self.bc.xn_value
             self.is_xn_bc_applied = True
 
         if self.bc.x0_is_neumann:
-            h = self.grid_x[1]-self.grid_x[0]
+            h = self.grid_x[1] - self.grid_x[0]
             F = self.bc.x0_value
-            self.mtx[0,0] = -2.0*D/(h*h)
-            self.mtx[0,1] =  2.0*D/(h*h)
-            self.vec[0] =  2 * F * ( D/h - h*w);
+            self.mtx[0, 0] = -2.0 * D / (h * h)
+            self.mtx[0, 1] = 2.0 * D / (h * h)
+            self.vec[0] = 2 * F * (D / h - h * w)
             self.is_x0_bc_applied = True
 
         if self.bc.xn_is_neumann:
-            h = self.grid_x[self.num_x_nodes-1]-self.grid_x[self.num_x_nodes-2]
+            h = self.grid_x[self.num_x_nodes - 1] - self.grid_x[self.num_x_nodes - 2]
             F = self.bc.xn_value
-            self.mtx[self.num_x_nodes-1, self.num_x_nodes-2] = -2.0*D/(h*h)
-            self.mtx[self.num_x_nodes-1, self.num_x_nodes-1] = 2.0*D/(h*h)
-            self.vec[self.num_x_nodes-1] =  2 * F * ( D/h - h*w);
+            self.mtx[self.num_x_nodes - 1, self.num_x_nodes - 2] = -2.0 * D / (h * h)
+            self.mtx[self.num_x_nodes - 1, self.num_x_nodes - 1] = 2.0 * D / (h * h)
+            self.vec[self.num_x_nodes - 1] = 2 * F * (D / h - h * w)
             self.is_xn_bc_applied = True
 
         assert self.is_x0_bc_applied and self.is_xn_bc_applied
 
+
 class BvpOde2D(BvpOde1D):
+
     """docstring for BvpOde2D"""
+
     def __init__(self, ode, bc, num_x_nodes, num_y_nodes):
         BvpOde1D.__init__(self, ode, bc, num_x_nodes)
         self.num_y_nodes = num_y_nodes
@@ -172,5 +176,3 @@ class BvpOde2D(BvpOde1D):
 
     def populate_matrix(self):
         pass
-
-
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from __future__ import division # normal division
+from __future__ import division  # normal division
 import nose.tools as test
 from nose.tools import nottest
 from nose.plugins.skip import Skip, SkipTest
@@ -20,23 +20,29 @@ import sys
 import ast
 import re
 
+
 def model_prob_1(x):
     return 1.0
+
 
 def model_prob_2(x):
     return 34.0 * math.sin(x)
 
+
 def solution_prob_1(x):
-    return 0.5*x*(1-x)
+    return 0.5 * x * (1 - x)
+
 
 def solution_prob_2(x):
-    return (4*math.exp(x) + math.exp(-4*x))/(4*math.exp(math.pi)+math.exp(-4*math.pi))-5*math.sin(x)-3*math.cos(x)
+    return (4 * math.exp(x) + math.exp(-4 * x)) / (4 * math.exp(math.pi) + math.exp(-4 * math.pi)) - 5 * math.sin(x) - 3 * math.cos(x)
+
 
 def init_cond(x):
-    return 0.5*x
+    return 0.5 * x
 
 
 class TestBoundaryConditions:
+
     def initilization_1D_test(cls):
         bc0 = BoundaryConditions1D()
         test.eq_(bc0.x0_is_dirichlet, False)
@@ -90,7 +96,6 @@ class TestBoundaryConditions:
         test.eq_(bc1.y0_is_neumann, False)
         test.eq_(bc1.yn_is_neumann, False)
 
-
     def neumann_2D_test(cls):
         bc1 = BoundaryConditions2D()
         bc1.set_x0_neumann_bc(0.15)
@@ -108,92 +113,98 @@ class TestBoundaryConditions:
         test.eq_(bc1.y0_is_neumann, True)
         test.eq_(bc1.yn_is_neumann, True)
 
+
 class TestODE:
+
     def initialization_test(cls):
-        ode1 = SecondOrderOde1D(10, -10, 0, model_prob_1, 0, 0 , 15) 
-        test.eq_(ode1.uxx,   10)
-        test.eq_(ode1.ux,   -10)
-        test.eq_(ode1.u,      0)
-        test.eq_(ode1.x_min,  0)
+        ode1 = SecondOrderOde1D(10, -10, 0, model_prob_1, 0, 0, 15)
+        test.eq_(ode1.uxx, 10)
+        test.eq_(ode1.ux, -10)
+        test.eq_(ode1.u, 0)
+        test.eq_(ode1.x_min, 0)
         test.eq_(ode1.x_max, 15)
 
+
 class TestGrid:
+
     def initialization_of_1D_test(cls):
-        ode1 = SecondOrderOde1D(10, -10, 0, model_prob_1, 0, 0 , 15)
+        ode1 = SecondOrderOde1D(10, -10, 0, model_prob_1, 0, 0, 15)
         bc1 = BoundaryConditions1D()
         bc1.set_x0_dirichlet_bc(0.15)
         bc1.set_xn_neumann_bc(0)
         bvp1 = BvpPde1D(ode1, bc1, 0.01, 0, 10, 120, init_cond)
-        test.eq_((bvp1.grid_x==np.linspace(0, 15, 120)).all(),True)
-    
+        test.eq_((bvp1.grid_x == np.linspace(0, 15, 120)).all(), True)
+
+
 class TestAccuracyOfSolution:
+
     def solution_variance_of_problem_1_test(cls):
         n = 128
-        ode1 = SecondOrderOde1D(-1, 0.0, 0.0, model_prob_1, 0.0, 0 , 1)
+        ode1 = SecondOrderOde1D(-1, 0.0, 0.0, model_prob_1, 0.0, 0, 1)
         bc1 = BoundaryConditions1D()
         bc1.set_x0_dirichlet_bc(0)
         bc1.set_xn_dirichlet_bc(0)
         bvp1 = BvpOde1D(ode1, bc1, n)
         bvp1.solve()
         sum = 0
-        for i in xrange(0,128):
+        for i in xrange(0, 128):
             x = bvp1.grid_x[i]
-            sum += math.pow((bvp1.U[i] - solution_prob_1(x)),2)
-        sum/=n-1
-        test.eq_((math.sqrt(sum)<math.pow(10,-10)),True)
+            sum += math.pow((bvp1.U[i] - solution_prob_1(x)), 2)
+        sum /= n - 1
+        test.eq_((math.sqrt(sum) < math.pow(10, -10)), True)
 
     def solution_variance_of_problem_2_test(cls):
         n = 128
-        ode1 = SecondOrderOde1D(1, 3.0, -4.0, model_prob_2, 0.0, 0 , math.pi)
+        ode1 = SecondOrderOde1D(1, 3.0, -4.0, model_prob_2, 0.0, 0, math.pi)
         bc1 = BoundaryConditions1D()
         bc1.set_x0_neumann_bc(-5.0)
         bc1.set_xn_dirichlet_bc(4.0)
         bvp1 = BvpOde1D(ode1, bc1, n)
         bvp1.solve()
-        sum = 0    
-        for i in xrange(0,n):
+        sum = 0
+        for i in xrange(0, n):
             x = bvp1.grid_x[i]
-            sum += math.pow((bvp1.U[i] - solution_prob_2(x)),2)
-        sum/=(n-1)
-        test.eq_((math.sqrt(sum)<math.pow(10,-2)),True)
+            sum += math.pow((bvp1.U[i] - solution_prob_2(x)), 2)
+        sum /= (n - 1)
+        test.eq_((math.sqrt(sum) < math.pow(10, -2)), True)
 
     def with_custom_grid_test(cls):
         n = 128
-        grid = np.hstack((np.linspace(0, 1, 64, endpoint=False), np.linspace(1, 2, 48,endpoint=False), np.linspace(2, math.pi, 16, endpoint=True)))
-        ode1 = SecondOrderOde1D(1, 3.0, -4.0, model_prob_2, 0.0, 0 , math.pi)
+        grid = np.hstack((np.linspace(0, 1, 64, endpoint=False), np.linspace(1, 2, 48, endpoint=False), np.linspace(2, math.pi, 16, endpoint=True)))
+        ode1 = SecondOrderOde1D(1, 3.0, -4.0, model_prob_2, 0.0, 0, math.pi)
         bc1 = BoundaryConditions1D()
         bc1.set_x0_neumann_bc(-5.0)
         bc1.set_xn_dirichlet_bc(4.0)
         bvp1 = BvpOde1D(ode1, bc1, n, True, grid)
         bvp1.solve()
-        sum = 0    
-        for i in xrange(0,n):
+        sum = 0
+        for i in xrange(0, n):
             x = bvp1.grid_x[i]
-            sum += math.pow((bvp1.U[i] - solution_prob_2(x)),2)
-        sum/=(n-1)
-        test.eq_((math.sqrt(sum)<math.pow(10,-2)),True)
+            sum += math.pow((bvp1.U[i] - solution_prob_2(x)), 2)
+        sum /= (n - 1)
+        test.eq_((math.sqrt(sum) < math.pow(10, -2)), True)
 
 
 class TestSpecieCollector:
+
     def add_method_creates_container_with_specie_test(cls):
         species = SpecieCollector()
         species.create_pde = MagicMock()
-        species.add_specie('Ox',1,2,3,4,5,6,7,8,9,10,11,12)
-        test.eq_(species.all['Ox']['D']                    == 1, True)
-        test.eq_(species.all['Ox']['w']                    == 2, True)
-        test.eq_(species.all['Ox']['dt']                   == 3, True)
-        test.eq_(species.all['Ox']['T']                    == 4, True)
-        test.eq_(species.all['Ox']['bc_x0_type']           == 5, True)
-        test.eq_(species.all['Ox']['bc_x0_value']          == 6, True)
-        test.eq_(species.all['Ox']['bc_xn_type']           == 7, True)
-        test.eq_(species.all['Ox']['bc_xn_value']          == 8, True)
-        test.eq_(species.all['Ox']['init_concentrations']  == 9, True)
-        test.eq_(species.all['Ox']['x_min']                == 10, True)
-        test.eq_(species.all['Ox']['x_max']                == 11, True)
-        test.eq_(species.all['Ox']['num_x_nodes']          == 12, True)
+        species.add_specie('Ox', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+        test.eq_(species.all['Ox']['D'] == 1, True)
+        test.eq_(species.all['Ox']['w'] == 2, True)
+        test.eq_(species.all['Ox']['dt'] == 3, True)
+        test.eq_(species.all['Ox']['T'] == 4, True)
+        test.eq_(species.all['Ox']['bc_x0_type'] == 5, True)
+        test.eq_(species.all['Ox']['bc_x0_value'] == 6, True)
+        test.eq_(species.all['Ox']['bc_xn_type'] == 7, True)
+        test.eq_(species.all['Ox']['bc_xn_value'] == 8, True)
+        test.eq_(species.all['Ox']['init_concentrations'] == 9, True)
+        test.eq_(species.all['Ox']['x_min'] == 10, True)
+        test.eq_(species.all['Ox']['x_max'] == 11, True)
+        test.eq_(species.all['Ox']['num_x_nodes'] == 12, True)
         species.create_pde.assert_called_once_with(species.all['Ox'])
 
-    
     def creating_PDEs_automatically_test(cls):
         x_min = 0
         x_max = 20
@@ -211,14 +222,13 @@ class TestSpecieCollector:
         bc_xn_value = 0
         init_concentrations = init_cond
         container = SpecieCollector()
-        container.add_specie('ox',d_ox, w_ox, dt, T, bc_x0_type, bc_x0_value, bc_xn_type, bc_xn_value, init_concentrations, x_min, x_max, num_x_nodes)
+        container.add_specie('ox', d_ox, w_ox, dt, T, bc_x0_type, bc_x0_value, bc_xn_type, bc_xn_value, init_concentrations, x_min, x_max, num_x_nodes)
         assert isinstance(container.all['ox']['pde'], BvpPde1D)
-        
+
         # Testing of updating of dictionary of species
-        for x in xrange(1,10):
+        for x in xrange(1, 10):
             container.differentiate1Ts()
         test.eq_((container.get_C_vector('ox') == container.dict_of_conc_vec['ox']).all(), True)
-
 
     def differentitate_1_time_step_for_each_specie_in_the_container_test(cls):
         pde_stub = MagicMock()
@@ -234,23 +244,23 @@ class TestSpecieCollector:
         species = SpecieCollector()
         pde_stub = MagicMock()
         species.all['ox'] = {'pde': pde_stub}
-        species.all['ox']['pde'].Ut = np.array([0.5,0.5])
-        test.eq_((species.get_C_vector('ox')==np.array([0.5, 0.5])).all(), True)
+        species.all['ox']['pde'].Ut = np.array([0.5, 0.5])
+        test.eq_((species.get_C_vector('ox') == np.array([0.5, 0.5])).all(), True)
 
     def update_dictionary_of_concentrations_test(cls):
         species = SpecieCollector()
         pde_stub = MagicMock()
         species.all['ox'] = {'pde': pde_stub}
-        species.all['ox']['pde'].Ut = np.array([0.5,0.5])
+        species.all['ox']['pde'].Ut = np.array([0.5, 0.5])
         species.dict_of_conc_vec['ox'] = 0
         species.update_dict_of_conc()
-        test.eq_((species.dict_of_conc_vec['ox'] == np.array([0.5,0.5]) ).all(), True)
+        test.eq_((species.dict_of_conc_vec['ox'] == np.array([0.5, 0.5])).all(), True)
 
     def add_current_specie_to_dictionary_of_conctreations_test(cls):
         species = SpecieCollector()
         species.create_pde = MagicMock()
         species.add_to_dict_of_conc = MagicMock()
-        species.add_specie('Ox',1,2,3,4,5,6,7,8,9,10,11,12)
+        species.add_specie('Ox', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
         species.add_to_dict_of_conc.assert_called_once_with('Ox')
 
     def add_to_dictionary_of_concentration_test(cls):
@@ -264,13 +274,13 @@ class TestSpecieCollector:
     def adding_reaction_term_at_each_time_step_test(cls):
         species = SpecieCollector()
         pde_stub = MagicMock()
-        
+
         species.all['ox'] = {'pde': pde_stub}
         species.all['om'] = {'pde': pde_stub}
-        
+
         species.all['ox']['pde'].U = 0.5
         species.all['om']['pde'].U = 0.5
-        
+
         species.all['ox']['rate'] = 'k*ox'
         species.all['om']['rate'] = 'k*ox'
 
@@ -278,43 +288,34 @@ class TestSpecieCollector:
         species.reaction_term()
         # raise SkipTest
 
-
     def create_rate_law_formulas_for_each_specie_based_on_regex_test(cls):
         species = SpecieCollector()
         pde_stub = MagicMock()
 
         species.all['ox'] = {'pde': pde_stub}
         species.all['om'] = {'pde': pde_stub}
-        A=1
-        b=0.5
-        x=100
+        A = 1
+        b = 0.5
+        x = 100
         species.all['ox']['rate'] = 'A*b/(x*x)'
         R_ox = eval(species.all['ox']['rate'])
 
         # raise SkipTest
-        # 
+        #
 
     def diffentiate_reaction_term_test(cls):
         # rate = '(a+b)/c'
         # var = {'a':np.array([1,2]),'b':np.array([2]),'c':np.array([3])}
         # result = ne.evaluate(rate, local_dict=var)
         # print result
+
         species = SpecieCollector()
         pde_stub = MagicMock()
 
         species.all['ox'] = {'pde': pde_stub}
         species.all['om'] = {'pde': pde_stub}
-        A=1
-        b=0.5
-        x=100
+        A = 1
+        b = 0.5
+        x = 100
         species.all['ox']['rate'] = 'A*b/(x*x)'
         R_ox = eval(species.all['ox']['rate'])
-
-
-
-        
-
-
-
-
-        
